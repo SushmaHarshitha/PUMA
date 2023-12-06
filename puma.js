bf5f473e6e5edafec8d422d5861147e3145009ad
V.Sushma Harshitha <142730693+SushmaHarshitha@users.noreply.github.com>
2023-12-06 16:32:15 +0530

    Add files via upload

---
+++ puma.js
@@ -0,0 +1,185 @@
+let login=document.getElementById("login")
+console.log(login);
+login.onclick=function(){
+    let login1=document.getElementById("login1")
+    console.log(login1);
+    let res1=login1.classList.toggle("true")
+    if(res1){
+        login1.style.display="none"
+    }else{
+        login1.style.display="block"
+    }
+}
+
+let search=document.getElementById("search")
+console.log(search);
+search.onclick=function(){
+    let search1=document.getElementById("search1")
+    console.log(search1);
+    let res=search1.classList.toggle("true")
+    if(res){
+        search1.style.display="none"
+    }else{
+        search1.style.display="block"
+    }
+}
+
+let new1=document.getElementById("new1")
+console.log(new1);
+new1.onmouseover=function(){
+    let new11=document.getElementById("new11")
+    console.log(new11);
+    new11.style.visibility="visible"
+    new11.style.height="550px"
+    new11.style.transition="ease all 2s"
+    new11.style.zIndex="11111"
+}
+
+new1.onmouseleave=function(){
+    let new11=document.getElementById("new11")
+    console.log(new11);
+    new11.style.visibility="hidden"
+    new11.style.height="0px"
+    new11.style.transition="ease all 2s"
+    new11.style.zIndex="1111"
+}
+
+let women=document.getElementById("women")
+console.log(women);
+women.onmouseover=function(){
+    let women1=document.getElementById("women1")
+    console.log(women1);
+    women1.style.visibility="visible"
+    women1.style.height="590px"
+    women1.style.transition="ease all 2s"
+    women1.style.zIndex="11111"
+}
+
+women.onmouseleave=function(){
+    let women1=document.getElementById("women1")
+    console.log(women1);
+    women1.style.visibility="hidden"
+    women1.style.height="0px"
+    women1.style.transition="ease all 2s"
+    women1.style.zIndex="111111"
+}
+
+let men=document.getElementById("men")
+console.log(men);
+men.onmouseover=function(){
+    let men1=document.getElementById("men1")
+    console.log(men1);
+    men1.style.visibility="visible"
+    men1.style.height="590px"
+    men1.style.transition="ease all 2s"
+    men1.style.zIndex="11111"
+}
+
+men.onmouseleave=function(){
+    let women1=document.getElementById("men1")
+    console.log(men1);
+    men1.style.visibility="hidden"
+    men1.style.height="0px"
+    men1.style.transition="ease all 2s"
+    men1.style.zIndex="111111"
+}
+
+let kid=document.getElementById("kid")
+console.log(kid);
+kid.onmouseover=function(){
+    let kid1=document.getElementById("kid1")
+    console.log(kid1);
+    kid1.style.visibility="visible"
+    kid1.style.height="500px"
+    kid1.style.transition="ease all 2s"
+    kid1.style.zIndex="11111"
+}
+
+kid.onmouseleave=function(){
+    let kid1=document.getElementById("kid1")
+    console.log(kid1);
+    kid1.style.visibility="hidden"
+    kid1.style.height="0px"
+    kid1.style.transition="ease all 2s"
+    kid1.style.zIndex="111111"
+}
+
+let sport=document.getElementById("sport")
+console.log(sport);
+sport.onmouseover=function(){
+    let sport1=document.getElementById("sport1")
+    console.log(sport1);
+    sport1.style.visibility="visible"
+    sport1.style.height="230px"
+    sport1.style.transition="ease all 2s"
+    sport1.style.zIndex="11111"
+}
+
+sport.onmouseleave=function(){
+    let sport1=document.getElementById("sport1")
+    console.log(sport1);
+    sport1.style.visibility="hidden"
+    sport1.style.height="0px"
+    sport1.style.transition="ease all 2s"
+    sport1.style.zIndex="111111"
+}
+
+let collab=document.getElementById("collab")
+console.log(collab);
+collab.onmouseover=function(){
+    let collab1=document.getElementById("collab1")
+    console.log(women1);
+    collab1.style.visibility="visible"
+    collab1.style.height="550px"
+    collab1.style.transition="ease all 2s"
+    collab1.style.zIndex="11111"
+}
+
+collab.onmouseleave=function(){
+    let collab1=document.getElementById("collab1")
+    console.log(collab1);
+    collab1.style.visibility="hidden"
+    collab1.style.height="0px"
+    collab1.style.transition="ease all 2s"
+    collab1.style.zIndex="111111"
+}
+
+let sports=document.getElementById("sports")
+console.log(sports);
+sports.onmouseover=function(){
+    let sports1=document.getElementById("sports1")
+    console.log(sports1);
+    sports1.style.visibility="visible"
+    sports1.style.height="450px"
+    sports1.style.transition="ease all 2s"
+    sports1.style.zIndex="11111"
+}
+
+sports.onmouseleave=function(){
+    let sports1=document.getElementById("sports1")
+    console.log(sports1);
+    sports1.style.visibility="hidden"
+    sports1.style.height="0px"
+    sports1.style.transition="ease all 2s"
+    sports1.style.zIndex="111111"
+}
+
+let out=document.getElementById("out")
+console.log(out);
+out.onmouseover=function(){
+    let out1=document.getElementById("out1")
+    console.log(out1);
+    out1.style.visibility="visible"
+    out1.style.height="550px"
+    out1.style.transition="ease all 2s"
+    out1.style.zIndex="11111"
+}
+
+out.onmouseleave=function(){
+    let out1=document.getElementById("out1")
+    console.log(out1);
+    out1.style.visibility="hidden"
+    out1.style.height="0px"
+    out1.style.transition="ease all 2s"
+    out1.style.zIndex="111111"
+}
